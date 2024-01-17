f882433c9a33c2b4ebac7250e179077f54c1f7c7
Yaser Asaad <YaserAsaad500@gmail.com>
2024-01-17 18:22:15 +0300

    LINUX

--- src/App.jsx
+++ src/App.jsx
@@ -29,11 +29,11 @@ function App() {
 
         {/* <button
           onClick={handleAddTodo}
-          class="inline-block rounded-md w-fit text-center h-fit border border-indigo-600 bg-indigo-600 p-2 self-center cursor-pointer text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 text-2xl"
+          className="inline-block rounded-md w-fit text-center h-fit border border-indigo-600 bg-indigo-600 p-2 self-center cursor-pointer text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 text-2xl"
         >
           ADD
         </button> */}
-        <CustomButton />
+        {<CustomButton action={handleAddTodo} />}
       </div>
 
       <ul className="py-4">
--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function CustomButton(handleAddTodo) {
+function CustomButton({ action }) {
   return (
     <div>
       <button
-        onClick={handleAddTodo}
-        class="inline-block rounded-md w-fit text-center h-fit border border-indigo-600 bg-indigo-600 p-2 self-center cursor-pointer text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 text-2xl"
+        onClick={action}
+        className="inline-block rounded-md w-fit text-center h-fit border border-indigo-600 bg-indigo-600 p-2 self-center cursor-pointer text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500 text-2xl"
       >
         ADD
       </button>
